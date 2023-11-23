a19cbc4dbded86a3949a6ae013e2cbadef53b386
Dominibo95 <dominibo95@gmail.com>
2023-11-23 22:28:25 +0000

    Zmiany do tej pory

--- CompanyApp/CompanyApp/Program.cs
+++ CompanyApp/CompanyApp/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using CompanyApp.Repositories;
+using CompanyApp.Entities;
+
+var employeeRepositories = new GenericRepository<Employee, int>();
+
+employeeRepositories.Add(new Employee { FirstName = "Dominik" });
+employeeRepositories.Add(new Employee { FirstName = "Adam" });
+employeeRepositories.Add(new Employee { FirstName = "Piotr" });
+
+employeeRepositories.Safe();
+
+Employee test = employeeRepositories.GetById(3);
+
+Console.WriteLine(test);
---
+++ CompanyApp/CompanyApp/Repositories/EmployeeRepositories.cs
@@ -0,0 +1,29 @@
+﻿using CompanyApp.Entities;
+using System.Xml.Serialization;
+
+namespace CompanyApp.Repositories
+{
+    public class EmployeeRepositories
+    {
+        private readonly List<Employee> _employees = new();
+
+        public void Add(Employee employee)
+        {
+            employee.Id = _employees.Count + 1;
+            _employees.Add(employee);
+        }
+
+        public void Safe()
+        {
+            foreach (var employee in _employees)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+
+        public Employee GetById(int id)
+        {
+            return _employees.Single(Employee => Employee.Id == id);
+        }
+    }
+}
